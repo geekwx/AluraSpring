9a92c3e7710f6f75dfd4e03cecb1fb7e1fe2d80f
Wesley Roberto <suporte@ubbonline.org.br>
2022-01-28 17:23:46 -0300

    alguns itens

--- Thymeleaf/mudi/src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ Thymeleaf/mudi/src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -3,15 +3,23 @@ package br.com.alura.mvc.mudi.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import br.com.alura.mvc.mudi.model.Pedido;
+import br.com.alura.mvc.mudi.repository.PedidoRepository;
 
 @Controller
 public class HomeController {
 
+	@Autowired
+	private PedidoRepository pedido;
 	
 	@GetMapping("/home")
 	public String home(Model model) {
@@ -25,6 +33,11 @@ public class HomeController {
 		List<Pedido> pedidos = Arrays.asList(pedido, pedido);
 		model.addAttribute("pedidos", pedidos);
 
+		
+		Query query = entityManager.createQuery("select p from Pedido p", Pedido.class);
+		List<Pedido> pedidos = query.getResultList();
+		
+		
 		return "home";
 	}
 }
