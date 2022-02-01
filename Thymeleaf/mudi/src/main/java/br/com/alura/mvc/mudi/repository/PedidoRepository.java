3ff5da63feb6932652f105257eb05997a0faffdb
GeekWx <geekwx@gmail.com>
2022-02-01 05:43:14 -0300

    04.02

--- Thymeleaf/mudi/src/main/java/br/com/alura/mvc/mudi/repository/PedidoRepository.java
+++ Thymeleaf/mudi/src/main/java/br/com/alura/mvc/mudi/repository/PedidoRepository.java
@@ -6,21 +6,18 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.alura.mvc.mudi.model.Pedido;
 
 
 @Repository 
-public class PedidoRepository {
+public interface PedidoRepository extends JpaRepository<Pedido, Long> {
 	
-	@PersistenceContext
-	private EntityManager entityManager;
+
 	
 	
-	public List<Pedido> recuperaTodosOspedidos(){
-		Query query = entityManager.createQuery("select p from Pedido p", Pedido.class);
-		return query.getResultList();
-	}
+
 
 }
